5d52a85480de9a793395a934ac43f6fa54ef8902
snehalin <55582815+snehalin@users.noreply.github.com>
2020-09-08 13:13:57 +0530

    Add files via upload

---
+++ IfDemo.java
@@ -0,0 +1,26 @@
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		int a=10;
+		
+		
+		 if(a==20) 
+		 { System.out.println("a==10"); } 
+		 System.out.println("hello");
+		 
+       
+       if(a==20);
+		{
+			System.out.println("a==10");
+		}
+       System.out.println("hii");
+       
+       if(10==10&&20==30||1==2) {
+       System.out.println("bye");
+       }
+      
+       System.out.println("java");
+	}
+
+}
---
+++ IfElse.java
@@ -0,0 +1,18 @@
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=30;
+		if(a>b)
+		{
+			System.out.println(a+"is greater");
+		}
+		else
+		{
+			System.out.println(b+"is greater");
+		}
+System.out.println("end");
+	}
+
+}
